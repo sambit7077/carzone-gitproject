56cf12eee4076d28ec784e61af32d30be135a381
sambit7077 <sambitpanda1697@gmail.com>
2024-11-24 19:04:35 +0530

    some minor changes

--- carzone/settings.py
+++ carzone/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'carzone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'carzone' / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
